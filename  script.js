f3dd063ae51eb116fde10ab24b246dda38121152
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-06-16 19:28:20 +0100

    Initial commit

---
+++  script.js
@@ -0,0 +1,6 @@
+'use strict';
+//element selection
+// add button
+const newtaskBtn = document.getElementsByClassName('btn'); 
+//
+
---
+++ landing-page/home.js
@@ -0,0 +1,6 @@
+
+
+const gigiCode = 12;
+
+if (gigiCode === 12) console.log('you jsut wrote JS!!');
+
